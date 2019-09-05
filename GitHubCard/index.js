471c4e56306a299607d1606c24108fdd18922e3e
James Grantham <james.r.grantham@gmail.com>
2019-09-05 13:06:44 +0100

    View account info

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,8 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/jgrantham')
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -29,6 +31,8 @@ const followersArray = [];
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
+
+          
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
